6f64d827de0a4b1f62c489e8c57242a203bfb88f
Stephen Ibanez <sibanez@g.hmc.edu>
2018-10-15 20:05:32 -0700

    Fixed bug in GPIFO implemenation, the remove function was always returning None.

--- GPIFO.py
+++ GPIFO.py
@@ -11,6 +11,56 @@ class GPIFO_tree(object):
             - shape : the shape of the tree
         """
         self.shape = shape
+        self.nodes = {} # map node ID to node object
+        self.root = self.make_tree(shape, None)
+
+    def make_tree(self, shape, parent):
+        """
+        Create a tree of GPIFOs
+        Inputs:
+            shape : shape of tree
+                    e.g. [2, 0, 1]
+                    e.g. [6, [4, 0, 1], [5, 2, 3]]
+        """
+        if len(shape) == 0:
+            return None
+        elif len(shape) > 0:
+            rootID = shape[0]
+            root = GPIFO_node(rootID, parent)
+            self.nodes[rootID] = root
+            for n in shape[1:]:
+                if type(n) == list:
+                    node = self.make_tree(n, root)
+                else:
+                    node = GPIFO_node(n, root)
+                self.nodes[node.ID] = node
+                root.add_child(node) 
+            return root
+
+    def insert(self, elem, ranks, groupIDs, nodeID):
+        """
+        Insert an element into the tree.
+        Inputs:
+            - elem : the element to insert
+            - ranks : the ranks to use for each insertion. ranks[0] is used for nodeID
+            - groupIDs : the groupIDs to use for each insertion. groupIDs[0] is used for nodeID
+            - nodeID : the leaf node ID to insert into
+        """
+        leaf_node = self.nodes[nodeID]
+        if len(leaf_node.children) != 0:
+            print >> sys.stderr, "ERROR: GPIFO_tree.insert: cannot insert into non-leaf node {}".format(nodeID)
+            sys.exit(1)
+        leaf_node.insert(elem, ranks, groupIDs)
+
+    def remove(self):
+        """
+        Remove an element from the tree
+        """
+        elem = self.root.remove()
+        return elem
+
+    def __str__(self):
+        return str(self.root)
 
 class GPIFO(object):
     """
@@ -79,13 +129,59 @@ class GPIFO(object):
             print >> sys.stderr, "ERROR: GPIFO.remove: cannot remove from empty GPIFO"
             sys.exit(1)
 
-    def __str__(self):
+    def __str__(self, level=0):
         """ Return string representation of GPIFO
         """
         elements = self.items.get_items()
         result = ''
         for elem_list in elements:
-            result += '{} : {}\n'.format(str(elem_list[0]), str(elem_list[1]))
+            result += '\t'*level + '{} : {}\n'.format(str(elem_list[0]), str(elem_list[1]))
+        return result
+
+class GPIFO_node(GPIFO):
+    """
+    A node of the GPIFO_tree
+    """
+    def __init__(self, ID, parent):
+        self.ID = ID
+        self.parent = parent
+        self.children = []
+        super(GPIFO_node, self).__init__()
+
+    def add_child(self, node):
+        self.children.append(node)
+
+    def insert(self, elem, ranks, groupIDs):
+        """
+        Insert elem into the node, then continue insertions towards the root
+        """
+        if len(ranks) != len(groupIDs) or len(ranks) == 0 or len(groupIDs) == 0:
+            print >> sys.stderr, "ERROR: GPIFO_node.insert: invalid ranks and/or groupIDs:\n{}\n{}".format(ranks, groupIDs)
+            sys.exit(1)
+        super(GPIFO_node, self).insert(elem, ranks[0], groupIDs[0])
+        if self.parent is not None:
+            self.parent.insert(self.ID, ranks[1:], groupIDs[1:])
+
+    def remove(self):
+        """
+        Remove an element from the node and recursively remove from children until the leaf node is reached.
+        Return the element removed from the leaf
+        """
+        elem = super(GPIFO_node, self).remove()
+        if len(self.children) > 0:
+            for child in self.children:
+                if child.ID == elem:
+                    return child.remove()
+            print >> sys.stderr, "ERROR: GPIFO_node.remove: unknown child {} for node {}".format(elem, self.ID)
+            sys.exit(1)
+        else:
+            return elem
+
+    def __str__(self, level=0):
+        result = 'Node {}:\n'.format(self.ID)
+        result += super(GPIFO_node, self).__str__(level) + '\n'
+        for child in self.children:
+            result += child.__str__(level+1)
         return result
 
 class PriorityQ(object):
--- GPIFO_test.py
+++ GPIFO_test.py
@@ -1,12 +1,42 @@
 
-from Gpifo import GPIFO
+from GPIFO import GPIFO_tree, GPIFO
 import random
 
-NUM_ELEMS = 10
+NUM_ELEMS = 5
 MAX_RANK = 10
 
 elements = ['a', 'b', 'c', 'd', 'e']
 groupIDs = [1, 2, None]
+
+def make_ranks(num_ranks):
+    ranks = []
+    for i in range(num_ranks):
+        r = random.randint(0, MAX_RANK)
+        ranks.append(r)
+    return ranks
+
+def make_groupIDs(num_groupIDs):
+    IDs = []
+    for i in range(num_groupIDs):
+        groupID = random.choice(groupIDs)
+        IDs.append(groupID)
+    return IDs
+
+def test_GPIFO_tree():
+    depth = 2
+    tree = GPIFO_tree([2, 0, 1])
+    for i in range(NUM_ELEMS):
+        elem = random.choice(elements)
+        ranks = make_ranks(depth)
+        gIDs = make_groupIDs(depth)
+        leaf_node = random.randint(0, 1)
+        print 'Inserting element: {} -- ranks: {} -- groupIDs: {} -- leaf_node: {}'.format(elem, ranks, gIDs, leaf_node)
+        tree.insert(elem, ranks, gIDs, leaf_node)
+    print 'Current tree state:\n{}'.format(str(tree))
+    for i in range(NUM_ELEMS):
+        head = tree.remove()
+        print 'Removed element: {}'.format(head)
+    print 'FINISHED!'   
 
 def test_GPIFO():
     gpifo = GPIFO()
@@ -23,7 +53,8 @@ def test_GPIFO():
     print 'FINISHED!'
 
 def main():
-    test_GPIFO()
+    #test_GPIFO()
+    test_GPIFO_tree()
 
 if __name__ == '__main__':
     main()
